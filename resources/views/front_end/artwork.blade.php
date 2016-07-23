cdb43e29bb91fe1cc3687fcefe3f7a3eac33dd7b
DJSweetness <davidsimoneaujr@gmail.com>
2016-07-23 04:17:44 +0000

    CSS and artwork main page side bar implementation

--- resources/views/front_end/artwork.blade.php
+++ resources/views/front_end/artwork.blade.php
@@ -1,17 +1,17 @@
 @extends('layouts.master')
 
 @section('styles')
-    <link rel="stylesheet" href="{{ URL::secure('src/css/')}}" type="text/css" />
+    <link rel="stylesheet" href="{{ URL::secure('src/css/artwork.css')}}" type="text/css" />
 @endsection
 
 @section('content')
 
     <div class="container">
-        <div class="search_bar">
+        <div class="left_bar">
+            Find Art Here
             <form action="" method="post">
                 <div class="input_group">
-                    <label for="search">Search Through Art</label>
-                    <input type='text' name='search' id='search' />
+                    <input type='text' name='search' id='search' placeholder="Search Artwork" />
                     <button type="submit" class="btn">Search</button>
                     <input type="hidden" name="_token" value="Session::token()" />
                 </div>
