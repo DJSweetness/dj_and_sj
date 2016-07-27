29de55f10512caa28cae879ff986739dbdd143e6
DJSweetness <davidsimoneaujr@gmail.com>
2016-07-27 05:05:23 +0000

    Implements music page with: 	Song Lists 	Album Covers 	Link hovers 	CSS

--- resources/views/front_end/music.blade.php
+++ resources/views/front_end/music.blade.php
@@ -8,9 +8,34 @@
 
     <div class="container">
        <div class="albums">
+           
            <div class="album">
-               <img src=""></img>
-               <p class="song_list"></p>
+                <img src="{{ URL::secure('images/Album Cover.jpg')}}"></img>
+                <a href="https://play.spotify.com/artist/7CYNlZYIAhsdSfZriJFTR8">Listen On Spotify</a>
+                <ul class="song_list">
+                   <li>Follow Your Heart</li>
+                   <li>Can't Wait</li>
+                   <li>Footsteps</li>
+                   <li>Keep Writing On</li>
+               </ul>
+           </div>
+           
+           <div class="album">
+               <img src="{{ URL::secure('images/willingtodream.jpg')}}"></img>
+               <a href="https://play.spotify.com/artist/0j4W5qS0GXaaKCMB1WtDKI">Listen On Spotify</a>
+               <ul class="song_list">
+                   <li>Let Love In</li>
+                   <li>Runaway</li>
+                   <li>Flower Child</li>
+                   <li>Fire Me Up</li>
+                   <li>Don't Let Her Go</li>
+                   <li>Lights On Still Water</li>
+                   <li>We Believe</li>
+                   <li>The Way To Live</li>
+                   <li>Take This Day</li>
+                   <li>Cougar Bait</li>
+               </ul>
+            
            </div>
        </div>
     </div>
