795a80244e5159bcb70ca95b7e2f83c469912841
DJSweetness <davidsimoneaujr@gmail.com>
2016-07-28 00:24:03 +0000

    Fixes to index. Mobile images on index are now normal sized and not stretched out. Fixes to the bulkiness of the index.css and index.php.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,25 +6,16 @@
 
 @section('content')
 
-    <div class="container"><!--
-
-     --><div class="category">
-            Artwork
-            <div class="artwork">
-                <div class="images">
-                    <a href="{{ route('artwork') }}"><img src="{{ URL::secure('images/elephant.jpg') }}"></img></a>
-                </div>
-            </div>
-        </div><!-- 
-     --><div class="category">
-            Music
-            <div class="music">
-                <div class="images">
-                    <a href="{{ route('music') }}"><img src="{{ URL::secure('images/Album Cover.jpg') }}"></img></a>
-                </div>
-            </div>    
-        </div><!--
- --></div>
+    <div class="container">
         
-
+        <div class="category">
+            <text>Artwork</text>
+            <a href="{{ route('artwork') }}"><img src="{{ URL::secure('images/elephant.jpg') }}"></img></a>
+        </div>
+        
+        <div class="category">
+            <text>Music</text>
+            <a href="{{ route('music') }}"><img src="{{ URL::secure('images/Album Cover.jpg') }}"></img></a>
+        </div>
+    </div>
 @endsection
